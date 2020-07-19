757d81dc0dbe0d4cad537ff6fd49552344b55eae
jackjou0920 <jackjou0920@gmail.com>
2020-07-19 09:56:21 +0800

    Updates

--- precache-manifest.0ab80cbfacce2b635dcbe07f7d74409f.js
+++ precache-manifest.0ab80cbfacce2b635dcbe07f7d74409f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "72e311b031e85088bdc88d0f20c6cfd8",
+    "revision": "a895c716eede32e5c3ea6aaeb20438b8",
     "url": "/react-portfolio/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-portfolio/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "36d2ca67aefb7178c66c",
-    "url": "/react-portfolio/static/css/main.74f8f2b9.chunk.css"
+    "revision": "bcbb7398f1a3d04340d9",
+    "url": "/react-portfolio/static/css/main.529d2f52.chunk.css"
   },
   {
     "revision": "3a33e3c7786f54403212",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-portfolio/static/js/2.9a60602e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "36d2ca67aefb7178c66c",
+    "revision": "bcbb7398f1a3d04340d9",
     "url": "/react-portfolio/static/js/main.f5a4fd6e.chunk.js"
   },
   {
@@ -28,8 +28,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
   },
   {
-    "revision": "09044ac4bb4ce23ac4eee5b3dfa5ccc4",
-    "url": "/react-portfolio/static/media/background.09044ac4.jpg"
+    "revision": "86136af8302cb514947f0a9453278dae",
+    "url": "/react-portfolio/static/media/bg1024x545.86136af8.jpg"
   },
   {
     "revision": "8fb91b4519ff8da4144ce118951e50f6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.38d85e83892a1fb9da160a0529b680e7.js"
+  "/react-portfolio/precache-manifest.0ab80cbfacce2b635dcbe07f7d74409f.js"
 );
 
 self.addEventListener('message', (event) => {
